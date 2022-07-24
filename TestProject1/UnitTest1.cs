4f57ce150bedd85f5a521b3580be99de6ca090c3
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-24 12:09:24 +0530

    Calculate Far of Ride

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,44 @@
+using NUnit.Framework;
+using CabInvoiceGenerator;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        InvoiceGenerator invoiceGenerator_PREMIUM = null;
+        InvoiceGenerator invoiceGenerator_NORMAL = null;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            invoiceGenerator_PREMIUM = new InvoiceGenerator(RideType.PREMIUM);
+            invoiceGenerator_NORMAL = new InvoiceGenerator(RideType.NORMAL);
+        }
+
+        //Testing fare for Premium Ride
+        [Test]
+        public void GivenDistanceAndTimeShouldReturnTotalFareForPREMIUM_Ride()
+        {
+            double distance = 15.0;
+            int time = 15;
+
+            double fare_PREMIUM = invoiceGenerator_PREMIUM.CalculateFare(15.0, 15); //Premium Ride
+            double expected_PREMIUM = 255;
+
+            Assert.AreEqual(expected_PREMIUM, fare_PREMIUM); // for Premium Ride
+        }
+        //Testing fare for Normal Ride
+        [Test]
+        public void GivenDistanceAndTimeShouldReturnTotalFareForNORMAL_Ride()
+        {
+            double distance = 15.0;
+            int time = 15;
+
+            double fare_NORMAL = invoiceGenerator_NORMAL.CalculateFare(15.0, 15); //Normal Ride
+            double expected_Normal = 165;
+
+            Assert.AreEqual(expected_Normal, fare_NORMAL); // for Normal Ride
+        }
+    }
+}
